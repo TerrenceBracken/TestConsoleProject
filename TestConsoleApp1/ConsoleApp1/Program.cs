65ff1a31304690412192547803e0e7ac0e8a521b
Terrence Bracken <terrence.bracken@att.net>
2018-09-13 14:03:23 -0500

    2nd commit of changes

--- TestConsoleApp1/ConsoleApp1/Program.cs
+++ TestConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             //isnt this a change?
+            //next set of changes after the initial commit.
         }
     }
 }

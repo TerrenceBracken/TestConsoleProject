2637e92dd1067767433935549bb0e736e7115f1c
Terrence Bracken <terrence.bracken@att.net>
2018-09-13 14:22:28 -0500

    send this to remote

--- TestConsoleApp1/ConsoleApp1/Program.cs
+++ TestConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApp1
             //next set of changes after the initial commit.
             //3rd change now that i have finally pushed the project to Git.  Seriously?
 
+            //here is some work the server dont have.
+
         }
     }
 }

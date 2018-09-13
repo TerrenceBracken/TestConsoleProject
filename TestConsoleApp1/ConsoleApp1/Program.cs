1f355ad3d1c7501e5f419ca38cf8c1ef714ac0eb
Terrence Bracken <42147604+TerrenceBracken@users.noreply.github.com>
2018-09-13 14:11:00 -0500

    some other commit

--- TestConsoleApp1/ConsoleApp1/Program.cs
+++ TestConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
             //isnt this a change?
             //next set of changes after the initial commit.
             //3rd change now that i have finally pushed the project to Git.  Seriously?
-
+            //some change before the 4th commit that I don't have.  Was added after I commited 4th and did Sync, but not pushed yet.
         }
     }
 }
